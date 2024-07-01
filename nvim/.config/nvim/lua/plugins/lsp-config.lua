b313c521fd2c3ad0a52422629277d5377025115b
Josh Reuben <jreuben111@gmail.com>
2024-07-01 20:47:26 +0300

    typecraft nvim

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,64 +1,75 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup(
-                {
-                    ensure_installed = {
-                        "rust-analyzer",
-                        "lua_ls",
-                        "bashls",
-                        "clangd",
-                        "cmake",
-                        "cssls",
-                        "codeqlls",
-                        "cypher_ls",
-                        "dockerls",
-                        "eslint",
-                        "glslls",
-                        "graphql",
-                        "html",
-                        "htmx",
-                        "jsonls",
-                        "julials",
-                        "tsserver",
-                        "jinja_lsp",
-                        "jqls",
-                        "autotools_ls",
-                        "markdown_oxide",
-                        "pyright",
-                        "ruff",
-                        "sqlls",
-                        "taplo",
-                        "tailwindcss",
-                        "terraformls",
-                        "lemminx",
-                        "yamlls",
-                    }
-                }
-            )
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require('lspconfig')
-            lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup({})
-            lspconfig.tsserver.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"rust_analyzer",
+					"lua_ls",
+					"bashls",
+					"clangd",
+					"cmake",
+					"cssls",
+					"codeqlls",
+					"cypher_ls",
+					"dockerls",
+					"eslint",
+					"glslls",
+					"graphql",
+					"html",
+					"htmx",
+					"jsonls",
+					"julials",
+					"tsserver",
+					"jinja_lsp",
+					"jqls",
+					"autotools_ls",
+					"markdown_oxide",
+					"pyright",
+					"ruff",
+					"sqlls",
+					"taplo",
+					"tailwindcss",
+					"terraformls",
+					"lemminx",
+					"yamlls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.pyright.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({ capabilities = capabilities })
 
-            lspconfig.rust_analyzer.setup {
-              -- Server-specific settings. See `:help lspconfig-setup`
-              settings = {
-                ['rust-analyzer'] = {},
-              },
-            }
-        end
-    }
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				-- Server-specific settings. See `:help lspconfig-setup`
+				settings = {
+					["rust-analyzer"] = {
+						workspace = {
+							symbol = {
+								search = {
+									kind = "all_symbols",
+								},
+							},
+						},
+					},
+				},
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
