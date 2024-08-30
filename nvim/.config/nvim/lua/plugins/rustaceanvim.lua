3d6293308efad66df4a8935bc29bc8dce72e90c9
Josh Reuben <jreuben111@gmail.com>
2024-08-30 09:01:22 +0300

    nvim crates plugin

--- nvim/.config/nvim/lua/plugins/rustaceanvim.lua
+++ nvim/.config/nvim/lua/plugins/rustaceanvim.lua
@@ -11,4 +11,11 @@ return {
 		version = "^5", -- Recommended
 		lazy = false, -- This plugin is already lazy
 	},
+	{
+		"saecki/crates.nvim",
+		event = { "BufRead Cargo.toml" },
+		config = function()
+			require("crates").setup()
+		end,
+	},
 }
